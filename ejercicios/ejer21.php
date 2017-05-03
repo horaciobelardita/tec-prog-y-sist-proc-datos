bd5cbf1733a023c4615c834f49bef1bfd9ff2700
horacio <horaciobelardita@gmail.com>
2017-05-03 01:50:17 -0300

    fix ejercicio 21

--- ejercicios/ejer21.php
+++ ejercicios/ejer21.php
@@ -15,6 +15,8 @@
     <?php
     $archivo = fopen("ejer21.txt", "r");
     $articulos = array();
+    $total_entrada = 0;
+    $total_salida = 0;
     while (! feof($archivo)) {
         $linea = fgets($archivo);
         $campos = explode(",", $linea);
@@ -25,11 +27,14 @@
             'entrada' => $campos[3],
             'salida'  => $campos[4]
         ]);
+        $total_entrada += $campos[3];
+        $total_salida += $campos[4];
     }
     fclose($archivo);
     ?>
 
     <?php
+    // en el ejercicio pide cada 70 renglones seria reemplazar array_chunk($array, 70)
     foreach (array_chunk($articulos, 2) as $renglones) {
         echo "<table><tr><th>ID</th><th>Descripcion</th><th>Entrada</th><th>Salida</th><th>Total</th></tr>";
         echo "<tbody>";
@@ -44,6 +49,8 @@
         }
         echo "</tbody></table>";
     }
+    echo "Total de entrada: " . $total_entrada;
+    echo "Total salida: " . $total_salida;
     ?>
 
 </body>
