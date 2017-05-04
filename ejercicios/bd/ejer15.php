8131c55f96367db61d9297e50b681c200135e98f
horacio <horaciobelardita@gmail.com>
2017-05-04 12:25:18 -0300

    ejercicio 15 con bd

--- ejercicios/bd/ejer15.php
+++ ejercicios/bd/ejer15.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: horacio
- * Date: 5/4/17
- * Time: 9:43 AM
- */
+
+$dbopts = parse_url(getenv('DATABASE_URL'));
+
+$conn = new PDO('pgsql:host=' . $dbopts['host'] .';port=' . $dbopts['port'] .  ';dbname=' . ltrim($dbopts["path"],'/'), $dbopts['user'], $dbopts['pass']);
+
+// write sql query
+$sql = "SELECT * FROM ejer15";
+
+// execute query and store the results
+$cmd = $conn->prepare($sql);
+$cmd->execute();
+$alumnos = $cmd->fetchAll();
+// loop through the data
+foreach ($alumnos as $alumno) {
+    echo $alumno['nya'] . "<br>";
+}
+// display titles using echo
+
+// disconnect
+$conn = null;
--- ejercicios/bd/ejer15.sql
+++ ejercicios/bd/ejer15.sql
@@ -1,17 +1,16 @@
-create database  if not exists ejer15;
 
-use ejer15;
+CREATE TYPE sexo AS ENUM ('M', 'F');
+CREATE TYPE estado_civil AS ENUM ('S', 'C');
 
-create table personas (
-  id int auto_increment,
-  edad tinyint not null,
+create table ejer15 (
+  id serial,
+  edad int not null,
   nya varchar(100) not null,
-  sexo ENUM('M', 'F') NOT NULL DEFAULT 'M',
-  estado_civil ENUM('S', 'C') NOT NULL DEFAULT 'S',
-  PRIMARY KEY (id)
+  sexo sexo,
+  estado_civil estado_civil
 );
 
-INSERT INTO  personas (edad, nya, sexo, estado_civil) VALUES (15, 'Pepe', 'M', 'S');
-INSERT INTO  personas (edad, nya, sexo, estado_civil) VALUES (30, 'Maria', 'F', 'C');
-INSERT INTO  personas (edad, nya, sexo, estado_civil) VALUES (36, 'Gabriela', 'F', 'S');
-INSERT INTO  personas (edad, nya, sexo, estado_civil) VALUES (26, 'Jorge', 'M', 'S');
+INSERT INTO  ejer15 (edad, nya, sexo, estado_civil) VALUES (15, 'Pepe', 'M', 'S');
+INSERT INTO  ejer15 (edad, nya, sexo, estado_civil) VALUES (30, 'Maria', 'F', 'C');
+INSERT INTO  ejer15 (edad, nya, sexo, estado_civil) VALUES (36, 'Gabriela', 'F', 'S');
+INSERT INTO  ejer15 (edad, nya, sexo, estado_civil) VALUES (26, 'Jorge', 'M', 'S');
