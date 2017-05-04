3d81be897b72c001e80d556e51bbb51ed8490622
horacio <horaciobelardita@gmail.com>
2017-05-04 15:29:46 -0300

    ejercicio 15 con bd

--- ejercicios/bd/ejer15.php
+++ ejercicios/bd/ejer15.php
@@ -12,7 +12,24 @@ function get_cant_alumnos($conn) {
     return $cmd->fetch()['count'];
 }
 
+function get_cant_varones($conn) {
+    $sql = "SELECT count(*) FROM ejer15 WHERE sexo = 'M'";
+    $cmd = $conn->prepare($sql);
+    $cmd->execute();
+    return $cmd->fetch()['count'];
+}
+
+function get_cant_mujeres_solteras($conn) {
+    $sql = "SELECT count(*) FROM ejer15 WHERE sexo = 'F' and estado_civil = 'S'";
+    $cmd = $conn->prepare($sql);
+    $cmd->execute();
+    return $cmd->fetch()['count'];
+}
+
 echo "Cantidad de alumnos: " . get_cant_alumnos($conn) . "<br>";
+echo "Cantidad de varones: " . get_cant_varones($conn) . "<br>";
+echo "Cantidad de mujeres solteras: " . get_cant_mujeres_solteras($conn) . "<br>";
+
 
 
 // desconectar
