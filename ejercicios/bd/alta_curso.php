bbdc4ba5edd4a801522085f0696dd24c4f8532f1
horacio_b92 <horacio_b92@hotmail.com>
2017-05-04 23:06:59 +0000

    agregado ejemplo de alta de cursos

---
+++ ejercicios/bd/alta_curso.php
@@ -0,0 +1,15 @@
+<?php
+$dbopts = parse_url(getenv('DATABASE_URL'));
+
+$conn = new PDO('pgsql:host=' . $dbopts['host'] .';port=' . $dbopts['port'] .  ';dbname=' . ltrim($dbopts["path"],'/'), $dbopts['user'], $dbopts['pass']);
+
+$nombre_curso = $_POST['nombre_curso'];
+if (isset($nombre_curso)) {
+    if (trim($nombre_curso)) {
+     $sql = "insert into cursos (nombre_curso) values " . $nombre_curso;
+     $cmd = $conn->prepare($sql);
+     $cmd->execute();
+    }
+}
+
+$conn = null;
---
+++ ejercicios/bd/curso.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cursos</title>
+</head>
+<body>
+    <h2>Alta de curso</h2>
+    <form action="alta_curso.php" method="POST">
+        <input type="text" name="nombre_curso" />
+        <input type="submit" value="Alta Curso"/>
+    </form>
+    <h2>Alta de alumno</h2>
+    <?php include('obtener_cursos.php'); ?>
+    <form>
+        <input type="text" name="name"/>
+        <input type="text" name="email"/>
+        <select name="codigo_curso">
+            
+        </select>
+        <input type="submit" value="Submit"/>
+    </form>
+</body>
+</html>
